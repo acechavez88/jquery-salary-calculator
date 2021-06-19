c74dd368b3a4fe8954e9fe21dddb7e2406afc132
Abram Chavez <abram.chavez820@gmail.com>
2021-06-18 19:23:58 -0500

    added js notes for reference

--- client.js
+++ client.js
@@ -4,3 +4,36 @@ $(document).ready(onReady); // linking JQuery
 function onReady() {
     console.log("Here's to the freakin weekend");
 }
+
+
+//## Assignment
+
+// The application should have an input form that collects _employee first name, last name, ID number, job title, annual salary_.
+
+
+
+// A 'Submit' button should collect the form information, 
+
+
+
+//store the information to calculate monthly costs, 
+
+
+
+//append information to the DOM and clear the input fields. 
+
+
+
+//Using the stored information, calculate monthly costs and append this to the to DOM. 
+
+
+
+//add a red background color to the total monthly cost If the total monthly cost exceeds $20,000, 
+
+
+
+// Create a delete button that removes an employee from the DOM.
+
+
+
+//need to remove that Employee's salary from the reported total if removing an employee from the DOM.
